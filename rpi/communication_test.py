6a9a602ae4f85c760b651c1da9d10e0a16f254f9
vattenbuffel <noal@student.chalmers.se>
2019-10-09 18:20:57 +0200

    fix read-port

--- rpi/communication_test.py
+++ rpi/communication_test.py
@@ -2,7 +2,7 @@ import serial
 import RPi.GPIO as GPIO
 import time
 
-ser = serial.Serial("dev/ttyACM0", 9600)
+ser = serial.Serial("/dev/ttyUSB0", 9600)
 ser.baudrate = 9600
 
 GPIO.setmode(GPIO.BOARD)
