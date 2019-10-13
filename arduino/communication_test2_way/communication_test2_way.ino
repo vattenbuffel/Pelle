acc724c77cadadbd0987c4a8f82307ff2a1be089
vattenbuffel <noal@student.chalmers.se>
2019-10-13 12:21:03 +0200

    2-way communication works

--- arduino/communication_test2_way/communication_test2_way.ino
+++ arduino/communication_test2_way/communication_test2_way.ino
@@ -1,42 +1,56 @@
 int incomingByte = 0; // for incoming serial data
-char send_left = '1';
-char send_right = '2';
-char new_speeds = '3';
+String message = "a";
+char incoming_char = 'a';
 
 void setup() {
-  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+  Serial.begin(115200);
 }
 
 void loop() {
   // send data only when you receive data:
   if (Serial.available() > 0) {
-    // read the incoming byte:
-    incomingByte = Serial.read();
+    message = "";
+    
+    while (Serial.available() > 0) {
+      incoming_char = Serial.read();
+      if (!(incoming_char == '\r' || incoming_char == '\n')){
+          //Serial.println("incing message");
+          //Serial.print("incoming_char: ");
+          //Serial.print(incoming_char);
+          delay(1);
+          message += incoming_char;
+        }
 
-    // say what you got:
+      if(incoming_char == '\r'){
+        //Serial.println("cpirövenkuken är == \ r");
+      }
+      if(incoming_char == '\n'){
+        //Serial.println("cpirövenkuken är == \ n");
+      }
+      
+    }
     
-    
-    
-    if (incomingByte == send_left){
+    /*
+      if (incomingByte == send_left){
       Serial.print("I should: ");
       Serial.println("send_left");
-    }
-    else if (incomingByte == send_right){
+      }
+      else if (incomingByte == send_right){
       Serial.print("I should: ");
       Serial.println("send_right");
-    }
-    else if (incomingByte == new_speeds){
-      Serial.print("I should: ");
-      Serial.println("change speeds to");
-      Serial.println(Serial.parseInt());
-      Serial.println(Serial.parseInt());
       }
-    
-    Serial.print("recieved : ");
-    Serial.println(incomingByte);
-    
+      else if (incomingByte == new_speeds){
+      Serial.print("I should: ");
+      Serial.print("change speeds to: ");
+      Serial.print(Serial.parseInt());
+      Serial.print(", ");
+      Serial.println(Serial.parseInt());
+      Serial.print("arduino received: ");
+      Serial.println();
+      }
+    */
+    Serial.print("message: ");
+    Serial.println(message);
   }
-  //Serial.println("didn't recieve anything");
-  //delay(100);
-  //Serial.println(incomingByte); 
+
 }
